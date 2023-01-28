4a36710163ebf800e42a4d437d2f18354dee564e
Abhishek Pandey <98042094+abhishek2813@users.noreply.github.com>
2023-01-29 04:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 // complete the given function
-
-function palindrome(string){
- const len = string.length;
-
-    // loop through half of the string
-    for (let i = 0; i < len / 2; i++) {
-
-        // check if first and last string are same
-        if (string[i] !== string[len - 1 - i]) {
-            return false;
-        }
-    }
-    return true;
-}
+let checkPalindrome = (stringg) => {
+      return stringg === stringg.split("").reverse().join("");
+    };
+     
 module.exports = palindrome
